90800a63fead76bda0d48ec68632272bfed76bac
blackyselva <selvam17196@gmail.com>
2023-03-19 17:42:37 +0530

    Adding.

---
+++ main.js
@@ -0,0 +1,11 @@
+document.write("5. Swap two number using function");
+
+document.write("<br>");
+document.write("<br>");
+
+var a=prompt("Enter a swap first number");
+var b=prompt("Enter a swap Second number");
+function swap(x,y) {
+	document.write(b, " " ,a);
+}
+swap(a, b);
